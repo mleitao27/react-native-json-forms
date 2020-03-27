2895e051fd3b087d1ef37ec1bfb191d468181950
Miguel Leitão <mleitao27@gmail.com>
2020-03-27 21:22:58 +0000

    Second try react native.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Form = props=> {
+    return (
+        <View>
+            <Text>Hello World!</Text>
+        </View>
+    );
+};
