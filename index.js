4d589dfbb5b200127782e86bfbd50860616f3ea5
Miguel Leitão <mleitao27@gmail.com>
2020-03-27 19:04:09 +0000

    More config stuf.

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Form = () => {
+    return (
+        <View>
+            <Text>Hello Word!</Text>
+        </View>
+    );
+};
+
+export default Form;
